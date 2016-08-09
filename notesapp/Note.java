f2bfd5ae296d923ee7ad6a2c3bc0c688cbb244c7
jatinrajani <jatinraj15@gmail.com>
2016-08-09 23:57:23 +0530

    Notesapp

---
+++ notesapp/Note.java
@@ -0,0 +1,28 @@
+package com.example.jatin.notesapp;
+
+/**
+ * Created by jatin on 9/8/16.
+ */
+public class Note {
+    private String mTitle;
+    private String mDescription;
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmDescription() {
+
+        return mDescription;
+    }
+
+    public void setmDescription(String mDescription) {
+        this.mDescription = mDescription;
+    }
+
+
+}
